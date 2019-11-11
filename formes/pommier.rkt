1c3c69faa7bc6c3abd46bff4c0f25ca9f6ce641b
Jerome Toulouse <monsieur.toulouse@gmail.com>
2019-11-11 20:57:04 +0100

    Rename some functions.

--- formes/pommier.rkt
+++ formes/pommier.rkt
@@ -1,30 +1,31 @@
 (require 2htdp/image)
 
 (define FEUILLAGE
-  (place-image
-     (circle 10 "solid" "red")
-     120 170
-     (place-image
-       (circle 10 "solid" "red")
-       80 40
-       (place-image
+    (place-image
         (circle 10 "solid" "red")
-        40 110
-       (place-image
-         (circle 10 "solid" "red")
-         190 80
-         (circle 110 "solid" "green"))))))
+        120 170
+        (place-image
+            (circle 10 "solid" "red")
+            80 40
+            (place-image
+                (circle 10 "solid" "red")
+                40 110
+                (place-image
+                    (circle 10 "solid" "red")
+                    190 80
+                    (circle 110 "solid" "green"))))))
+
+(define TRONC
+    (rectangle 50 200 "solid" "brown"))
   
-(define IMAGE
-  (overlay/xy
-    FEUILLAGE
-    85 210
-    (rectangle 50 200 "solid" "brown")
-    ))
+(define ARBRE
+    (overlay/xy
+        FEUILLAGE
+        85 210
+        TRONC))
 
 
 (place-image
- IMAGE
- 250 250
- (empty-scene 500 500)
- )
+    ARBRE
+    250 250
+    (empty-scene 500 500))
